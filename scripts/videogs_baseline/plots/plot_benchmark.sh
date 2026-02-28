57591f36a196a25b1a5c5bb3964a1eb40278a6e9
Rajrup <rajrup.withbestwishes@gmail.com>
2026-02-27 20:38:13 -0800

    Updated VideoGS plot scripts

--- scripts/videogs_baseline/plots/plot_benchmark.sh
+++ scripts/videogs_baseline/plots/plot_benchmark.sh
@@ -1,20 +1,46 @@
 #!/bin/bash
 
-# Plot benchmark results for videogs_baseline
+# Plot benchmark results for VideoGS compression on QUEEN-trained models
 DATASET_NAME="HiFi4G_Dataset"
 SEQUENCE_NAME="4K_Actor1_Greeting"
-QP=22
 
-working_dir="/home/rajrup/Project/VideoGS"
+# VideoGS H.264 QP parameters (0=lossless, 51=worst)
+QP=22
+QFD=22
+QFR1=22
+QFR2=22
+QFR3=22
+QO=22
+QS=22
+QR=22
+
+VIDEOGS_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd)"
 data_path="/synology/rajrup/VideoGS"
 input_folder="${data_path}/train_output/${DATASET_NAME}/${SEQUENCE_NAME}/compression/videogs"
-plot_script_folder="${working_dir}/scripts/videogs_baseline/plots"
+plot_script_folder="${VIDEOGS_ROOT}/scripts/videogs_baseline/plots"
+
+QP_ARGS="--qp ${QP} --qfd ${QFD} --qfr1 ${QFR1} --qfr2 ${QFR2} --qfr3 ${QFR3} --qo ${QO} --qs ${QS} --qr ${QR}"
 
 # Plot compressed size breakdown + point counts
-python ${plot_script_folder}/plot_compressed_size.py --input_folder ${input_folder} --dataset_name ${DATASET_NAME} --sequence_name ${SEQUENCE_NAME} --qp ${QP} --output_folder ${plot_script_folder}
+python ${plot_script_folder}/plot_compressed_size.py \
+    --input_folder ${input_folder} \
+    --dataset_name ${DATASET_NAME} \
+    --sequence_name ${SEQUENCE_NAME} \
+    ${QP_ARGS} \
+    --output_folder ${plot_script_folder}
 
 # Plot compression/decompression time
-python ${plot_script_folder}/plot_compression_time.py --input_folder ${input_folder} --dataset_name ${DATASET_NAME} --sequence_name ${SEQUENCE_NAME} --qp ${QP} --output_folder ${plot_script_folder}
+python ${plot_script_folder}/plot_compression_time.py \
+    --input_folder ${input_folder} \
+    --dataset_name ${DATASET_NAME} \
+    --sequence_name ${SEQUENCE_NAME} \
+    ${QP_ARGS} \
+    --output_folder ${plot_script_folder}
 
 # Plot quality
-python ${plot_script_folder}/plot_quality.py --input_folder ${input_folder} --dataset_name ${DATASET_NAME} --sequence_name ${SEQUENCE_NAME} --qp ${QP} --output_folder ${plot_script_folder}
+python ${plot_script_folder}/plot_quality.py \
+    --input_folder ${input_folder} \
+    --dataset_name ${DATASET_NAME} \
+    --sequence_name ${SEQUENCE_NAME} \
+    ${QP_ARGS} \
+    --output_folder ${plot_script_folder}
